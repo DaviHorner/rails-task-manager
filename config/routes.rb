5c5064b07718f0822675582cbcb363fb74fa0910
Davi Horner <davihhcastro@gmail.com>
2016-08-09 15:47:12 -0300

    Add gets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
+  # resources :tasks
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
+
+#     tasks GET    /tasks(.:format)          tasks#index
+#           POST   /tasks(.:format)          tasks#create
+#  new_task GET    /tasks/new(.:format)      tasks#new
+# edit_task GET    /tasks/:id/edit(.:format) tasks#edit
+#      task GET    /tasks/:id(.:format)      tasks#show
+#           PATCH  /tasks/:id(.:format)      tasks#update
+#           PUT    /tasks/:id(.:format)      tasks#update
+#           DELETE /tasks/:id(.:format)      tasks#destroy
